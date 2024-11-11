556fc82b7504fdb4dbb677f3ea3ae2da4b1c11d4
Gordon Ferguson <gordon@gordonferguson.me>
2024-11-11 17:17:20 -0500

    testing colorscheme settings and added in zoxide

--- config/plugins.nix
+++ config/plugins.nix
@@ -832,42 +832,42 @@
   -- }
        })  '';
 
-  colorschemes.catppuccin = {
-    enable = true;
-    settings = {
-      no_bold = false;
-      no_italic = false;
-      no_underline = false;
-      transparent_background = true;
-      integrations = {
-        cmp = true;
-        noice = true;
-        notify = true;
-        neotree = true;
-        harpoon = true;
-        gitsigns = true;
-        which_key = true;
-        illuminate.enabled = true;
-        treesitter = true;
-        treesitter_context = true;
-        telescope.enabled = true;
-        indent_blankline.enabled = true;
-        mini.enabled = true;
-        native_lsp = {
-          enabled = true;
-          inlay_hints = {
-            background = true;
-          };
-          underlines = {
-            errors = ["underline"];
-            hints = ["underline"];
-            information = ["underline"];
-            warnings = ["underline"];
-          };
-        };
-      };
-    };
-  };
+  # colorschemes.catppuccin = {
+  #   enable = true;
+  #   settings = {
+  #     no_bold = false;
+  #     no_italic = false;
+  #     no_underline = false;
+  #     transparent_background = true;
+  #     integrations = {
+  #       cmp = true;
+  #       noice = true;
+  #       notify = true;
+  #       neotree = true;
+  #       harpoon = true;
+  #       gitsigns = true;
+  #       which_key = true;
+  #       illuminate.enabled = true;
+  #       treesitter = true;
+  #       treesitter_context = true;
+  #       telescope.enabled = true;
+  #       indent_blankline.enabled = true;
+  #       mini.enabled = true;
+  #       native_lsp = {
+  #         enabled = true;
+  #         inlay_hints = {
+  #           background = true;
+  #         };
+  #         underlines = {
+  #           errors = ["underline"];
+  #           hints = ["underline"];
+  #           information = ["underline"];
+  #           warnings = ["underline"];
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 
   # extraPlugins = with pkgs.vimPlugins; [
   #   vim-be-good
@@ -877,7 +877,7 @@
 
   extraPlugins = with pkgs.vimPlugins;
     [
-      vim-be-good
+      zoxide
       glow-nvim # Glow inside of Neovim
       clipboard-image-nvim
     ];

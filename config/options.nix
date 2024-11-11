2e4b2be6a792fad2563e3a8dc0d6300262d77245
Gordon Ferguson <gordon@gordonferguson.me>
2024-11-11 14:51:33 -0500

    ported options.lua

--- config/options.nix
+++ config/options.nix
@@ -5,6 +5,25 @@
     opts = {
       number = true;
       relativenumber = true;
+      splitright = true;
+      splitbelow = true;
+      wrap = false;
+      expandtab = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      incsearch = true;
+      inccommand = "split";
+      ignorecase = true;
+      clipboard = "unnamedplus";
+      scrolloff = 999;
+      virtualedit = "block";
+      undofile = true;
+      termguicolors = true;
+      swapfile = false;
+      pumblend = 10;
+      conceallevel = 1;
+      foldlevelstart = 99;
+      showcmd = true;
     };
   };
 }

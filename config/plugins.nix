c3ea0c4575b24caf14c896a617951d10a62dea3c
Gordon Ferguson <gordon@gordonferguson.me>
2026-01-28 10:58:34 -0500

    Syntax corrections over flake update part 2

--- config/plugins.nix
+++ config/plugins.nix
@@ -406,12 +406,6 @@
     # File tree
     neo-tree = {
       enable = true;
-      buffers = {
-        bindToCwd = false;
-        followCurrentFile = {
-          enabled = true;
-        };
-      };
       settings = {
         enable_diagnostics = true;
         enable_git_status = true;
@@ -419,6 +413,14 @@
         enable_refresh_on_write = true;
         close_if_last_window = true;
         popup_border_style = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
+        buffers = {
+          bind_to_cwd = {
+            enabled = false;
+          };
+          follow_current_file = {
+            enabled = true;
+          };
+        };
         window = {
           width = 40;
           height = 15;
@@ -711,7 +713,7 @@
       	-- The first register is the default register used as macro-slot after
       	-- startup.
       	slots = { "a", "b" },
-    
+      
       	mapping = {
       		startStopRecording = "q",
       		playMacro = "Q",
@@ -722,22 +724,22 @@
       		-- ⚠️ this should be a string you don't use in insert mode during a macro
       		addBreakPoint = "##",
       	},
-    
+      
       	-- Clears all macros-slots on startup.
       	clear = false,
-    
+      
       	-- Log level used for non-critical notifications; mostly relevant for nvim-notify.
       	-- (Note that by default, nvim-notify does not show the levels `trace` & `debug`.)
       	logLevel = vim.log.levels.INFO, -- :help vim.log.levels
-    
+      
       	-- If enabled, only essential notifications are sent.
       	-- If you do not use a plugin like nvim-notify, set this to `true`
       	-- to remove otherwise annoying messages.
       	lessNotifications = false,
-    
+      
       	-- Use nerdfont icons in the status bar components and keymap descriptions
       	useNerdfontIcons = true,
-    
+      
       	-- Performance optimzations for macros with high count. When `playMacro` is
       	-- triggered with a count higher than the threshold, nvim-recorder
       	-- temporarily changes changes some settings for the duration of the macro.
@@ -753,7 +755,7 @@
       			"InsertCharPre",
       		},
       	},
-    
+      
       	-- [experimental] partially share keymaps with nvim-dap.
       	-- (See README for further explanations.)
       	dapSharedKeymaps = false,
